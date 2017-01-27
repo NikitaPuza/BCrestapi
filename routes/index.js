d0c71d428136cc5f0981d667ed2044259da202be
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-01-26 22:02:27 -0600

    adding missing dependencies

--- routes/index.js
+++ routes/index.js
@@ -4,13 +4,9 @@ var https = require('https');
 var bodyParser = require('body-parser');
 
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Rest API' });
 });
-/* GET /send 
-app.post('/send', function(req, res, next) {
-	res.render('send', { title: 'request sent' });
-}); */
+
 
 module.exports = router;
