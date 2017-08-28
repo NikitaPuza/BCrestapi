759ef5fdc6f0433dce46a1eba3985bd0be9bee8a
Nikita Puzanenko <nikitapuzanenko@gmail.com>
2017-08-28 18:43:59 -0500

    adding promise based https request to start

--- routes/get.js
+++ routes/get.js
@@ -7,9 +7,8 @@ router.post('/', (req, res) => {
 	let credentials = 'nikitapuzanenko:314dc1b9bfac6db6d203c4b92e3563d941e40f54';
 	credentials = new Buffer(credentials).toString('base64');
 	credentials = 'Basic ' + credentials;
-	console.log(credentials);
 	const options = {
-		host: 'https://store-r8lu9.mybigcommerce.com',
+		host: 'store-r8lu9.mybigcommerce.com',
 		path: '/api/v2/time',
 		method: 'GET',
 		headers: {
